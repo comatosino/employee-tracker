aaef62acf8f8d759b8957f8f73bc4a3c66e89b13
Robert Adams <adamsiii.robert@gmail.com>
2021-04-25 19:28:06 -0700

    removed comments in connection object

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ const inquirer = require('inquirer'); // for managing user input
 
 const connection = mysql.createConnection({
   host: 'localhost',
-
-  // Your port, if not 3306
   port: 3306,
-
-  // Your username
   user: 'root',
-
-  // Be sure to update with your own MySQL password!
   password: 'password',
   database: 'employee_tracker_db',
 });
