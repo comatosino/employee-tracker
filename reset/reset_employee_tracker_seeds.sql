12bd77e201ff4fd0df6b7fe9a3dbe8c2c65a333a
Robert Adams <adamsiii.robert@gmail.com>
2021-04-26 17:47:17 -0700

    corrected seed role id

--- reset/reset_employee_tracker_seeds.sql
+++ reset/reset_employee_tracker_seeds.sql
@@ -40,7 +40,7 @@ INSERT INTO role
 VALUES 
 ("Sales Lead", 100000, 1), 
 ("Salesperson", 80000, 1),
-("Lead Engineer", 150000, 1),
+("Lead Engineer", 150000, 2),
 ("Software Engineer", 120000, 2),
 ("Accountant", 125000, 3),
 ("Legal Team Lead", 250000, 4),
