9815b17170b316f91ae17e116cc5b5208fa5146b
Robert Adams <adamsiii.robert@gmail.com>
2022-09-04 15:50:12 -0700

    Define constants for io values and switch keys

---
+++ io/constants.js
@@ -0,0 +1,27 @@
+// departments //////////////////////////////////////////////////////////////
+const VIEW_ALL_DEPARTMENTS = 'VIEW_ALL_DEPARTMENTS';
+const ADD_DEPARTMENT = 'ADD_DEPARTMENT';
+const DELETE_DEPARTMENT = 'DELETE_DEPARTMENT';
+
+// roles ////////////////////////////////////////////////////////////////////
+const VIEW_ALL_ROLES = 'VIEW_ALL_ROLES';
+const ADD_ROLE = 'ADD_ROLE';
+
+// employees ////////////////////////////////////////////////////////////////
+const VIEW_ALL_EMPLOYEES = 'VIEW_ALL_EMPLOYEES';
+const ADD_EMPLOYEE = 'ADD_EMPLOYEE';
+const UPDATE_EMPLOYEE_ROLE = 'UPDATE_EMPLOYEE_ROLE';
+
+// quit /////////////////////////////////////////////////////////////////////
+QUIT = 'QUIT';
+
+module.exports = {
+  VIEW_ALL_DEPARTMENTS,
+  ADD_DEPARTMENT,
+  DELETE_DEPARTMENT,
+  VIEW_ALL_ROLES,
+  ADD_ROLE,
+  VIEW_ALL_EMPLOYEES,
+  ADD_EMPLOYEE,
+  UPDATE_EMPLOYEE_ROLE,
+};
