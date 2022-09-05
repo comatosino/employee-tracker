f998705965b6d0739af4ce542d3a350df96f1101
Robert Adams <adamsiii.robert@gmail.com>
2022-09-04 17:02:39 -0700

    Move constants directly into module export obj

--- io/constants.js
+++ io/constants.js
@@ -1,28 +1,22 @@
-// departments //////////////////////////////////////////////////////////////
-const VIEW_ALL_DEPARTMENTS = 'VIEW_ALL_DEPARTMENTS';
-const ADD_DEPARTMENT = 'ADD_DEPARTMENT';
-const DELETE_DEPARTMENT = 'DELETE_DEPARTMENT';
-
-// roles ////////////////////////////////////////////////////////////////////
-const VIEW_ALL_ROLES = 'VIEW_ALL_ROLES';
-const ADD_ROLE = 'ADD_ROLE';
-
-// employees ////////////////////////////////////////////////////////////////
-const VIEW_ALL_EMPLOYEES = 'VIEW_ALL_EMPLOYEES';
-const ADD_EMPLOYEE = 'ADD_EMPLOYEE';
-const UPDATE_EMPLOYEE_ROLE = 'UPDATE_EMPLOYEE_ROLE';
-
-// quit /////////////////////////////////////////////////////////////////////
-EXIT = 'EXIT';
-
 module.exports = {
-  VIEW_ALL_DEPARTMENTS,
-  ADD_DEPARTMENT,
-  DELETE_DEPARTMENT,
-  VIEW_ALL_ROLES,
-  ADD_ROLE,
-  VIEW_ALL_EMPLOYEES,
-  ADD_EMPLOYEE,
-  UPDATE_EMPLOYEE_ROLE,
-  EXIT,
+  // departments //////////////////////////////////////////////////////////////
+  VIEW_ALL_DEPARTMENTS: 'VIEW_ALL_DEPARTMENTS',
+  ADD_DEPARTMENT: 'ADD_DEPARTMENT',
+  DELETE_DEPARTMENT: 'DELETE_DEPARTMENT',
+
+  // roles ////////////////////////////////////////////////////////////////////
+
+  VIEW_ALL_ROLES: 'VIEW_ALL_ROLES',
+  ADD_ROLE: 'ADD_ROLE',
+  UPDATE_ROLE_SALARY: 'UPDATE_ROLE_SALARY',
+  DELETE_ROLE: 'DELETE_ROLE',
+
+  // employees ////////////////////////////////////////////////////////////////
+
+  VIEW_ALL_EMPLOYEES: 'VIEW_ALL_EMPLOYEES',
+  ADD_EMPLOYEE: 'ADD_EMPLOYEE',
+  UPDATE_EMPLOYEE_ROLE: 'UPDATE_EMPLOYEE_ROLE',
+
+  // quit /////////////////////////////////////////////////////////////////////
+  EXIT: 'EXIT',
 };
